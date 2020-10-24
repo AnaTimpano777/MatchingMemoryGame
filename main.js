c285e05e5e597fd4f6d1a9b226d4415f4d8ccf32
AnaTimpano777 <632419@smcdsb.on.ca>
2020-10-24 19:25:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,136 @@
+var score = 0;
+var cards = new Array();
+
+cards[0] = "images/ace-spades.png";
+cards[1] = "images/ace-spades.png";
+cards[2] = "images/two-spades.png";
+cards[3] = "images/two-spades.png";
+cards[4] = "images/three-spades.png";
+cards[5] = "images/three-spades.png";
+cards[6] = "images/four-spades.png";
+cards[7] = "images/four-spades.png";
+cards[8] = "images/five-spades.png";
+cards[9] = "images/five-spades.png";
+cards[10] = "images/six-spades.png";
+cards[11] = "images/six-spades.png";
+
+resetField();
+
+$(document).ready(function() {
+    localStorage.name = prompt('What is thy name?');
+    if(localStorage.score[localStorage.name] != undefined) {
+        score = localStorage.score[localStorage.name];
+        $('#score').text(score);
+    }
+    $('#field img').on({
+    'click': function(){
+         if($('.flipped').length > 1) {
+            return false;   
+         }
+        setTimeout(function() {  
+                    $('.flipped').not('[src="images/silhouette.png"]').attr('src','images/back.png');
+                },3500); 
+        $(this).attr('src',$(this).data('card'));
+        $(this).addClass('flipped');
+        if($('.flipped').length > 1) {
+            if($('.flipped').eq(0).data('card') == $('.flipped').eq(1).data('card')) {
+                 $('.flipped').addClass('match');
+                setTimeout(function() {
+                    $('.flipped').attr('src','images/silhouette.png');
+                    $('.flipped').removeClass('match');
+                    $('.flipped').removeClass('flipped');
+                    checkWin();
+                },600);
+            }
+            else {
+                  $('.flipped').addClass('fail');
+                setTimeout(function() {  
+                    $('#field IMG').not('[src="images/silhouette.png"]').attr('src','images/back.png');
+                    $('.flipped').removeClass('fail');
+                    $('.flipped').removeClass('flipped');
+                },600);
+             }
+            
+        }
+    }
+});
+});
+
+function checkWin() {
+    if($('#field IMG').not('[src="images/silhouette.png"]').length == 0) {  
+        $("#win").show();
+        $('BODY').addClass('confetti');
+        $('[src="images/silhouette.png"]').hide();
+        score++;
+        localStorage.score[localStorage.name] = score;
+        $('#score').text(score);
+        setTimeout(function() {
+            if(confirm('Do you want to play again?')) {
+                resetField();   
+            }
+        },2500);
+    }
+}
+
+function resetField() {
+    $('[src="images/silhouette.png"]').show();
+    $("#win").hide();
+    $('BODY').removeClass('confetti');
+    $("#field IMG").show();
+    $('.flipped').removeClass('flipped');
+    $("#field IMG").attr('src','images/back.png');
+    $("#field IMG").removeClass('match');
+    
+   cards = shuffle(cards);
+
+$('#field IMG').each(function(counter) {
+    $(this).data('card', cards[counter])
+    
+    $(this).attr('src',$(this).data('card'));
+    $("#refresh").hide();
+});
+
+  setTimeout(function() {
+                    $('#field IMG').attr('src','images/back.png');
+                },2000);
+
+    
+}
+  $('#reset-button').click(function() {
+        if(confirm('If you confirm this message your score will reset!')) {
+    resetGame();
+        }
+    });
+function resetGame() {
+    delete localStorage.name;
+    localStorage.score = [];
+    score = 0;
+    $('#score').text(score);
+}
+    
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element..
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+//add border and make it stay, woop woop
+//Reset the score (button)
+//Once you click a card and it is flipped after a few seconds make it turn back to back.png
+//Have a person enter there name and remember their individual score attached to their name
+// localStorage.name = prompt('What is thy name?')
+
+
